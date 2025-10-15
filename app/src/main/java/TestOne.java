c5128bf2f8463beacd596ecc6366800b22323c19
SalihAbi2836 <s_oeztuerk24@stud.hwr-berlin.de>
2025-10-15 17:17:42 +0200

    Revert "Initial commit: Android Studio project setup"
    This reverts commit 4682e270866c5a241edd9812791564964217a335.

--- app/src/main/java/TestOne.java
+++ app/src/main/java/TestOne.java
@@ -1,5 +1,5 @@
 public class TestOne {
     public static void main(String[] args) {
-        System.out.println("Commit 1 - Erste Datei erstellt");
+        System.out.println("Commit 1 - Zweite Datei erstellt");
     }
 }
